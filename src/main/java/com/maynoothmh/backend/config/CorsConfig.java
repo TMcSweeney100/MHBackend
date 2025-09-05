319a3457e553033daf192314f3d693dee0f37626
TMcSweeney100 <timmcsweeney12@hotmail.com>
2025-09-05 21:37:59 +0100

    updated the api

--- src/main/java/com/maynoothmh/backend/config/CorsConfig.java
+++ src/main/java/com/maynoothmh/backend/config/CorsConfig.java
@@ -9,8 +9,8 @@ import org.springframework.web.servlet.config.annotation.*;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")                       // allow all paths
-        		.allowedOrigins("http://localhost:5173") // your React app
+        registry.addMapping("/**")                       
+        		.allowedOrigins("http://localhost:5173","https://<your-vercel-app>.vercel.app") 
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowCredentials(true);
     }
